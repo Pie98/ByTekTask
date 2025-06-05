58e41a4ead13036c6d67a3a3cc420c545ade44d8
Pie98 <pielotti1998@gmail.com>
2025-06-05 18:22:20 +0200

    fixed Dockerfile

--- bytek_task/api/main.py
+++ bytek_task/api/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
@@ -7,13 +8,17 @@ import time
 import logging
 import joblib
 import pandas as pd
-from circuit_breakers import circuit_breaker
-from rate_limiters import check_rate_limit
-from input_validator import FeatureValidationService
+from .circuit_breakers import circuit_breaker
+from .rate_limiters import check_rate_limit
+from .input_validator import FeatureValidationService
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-predictive_model = joblib.load('model/logistic_regression_model.pkl')
+
+# Get the directory where main.py is located
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, 'model', 'logistic_regression_model.pkl')
+predictive_model = joblib.load(model_path)
 
 
 class PropensityRequest(BaseModel):
@@ -113,21 +118,32 @@ async def predict_propensity_endpoint(
     Example:
         ```
         POST /predict-propensity
-        {
-            "user_id": "user123",
+          {
+            "user_id": "user_f8b7c861",
             "features": {
-                "age": 25.0,
-                "income": 50000.0,
-                "activity_score": 0.8
+              "tempo_ultimo_acquisto": 38.51593502067873,
+              "frequenza_visita_30gg": 71.0,
+              "valore_medio_carrello": 44.86634368525073,
+              "numero_acquisti_precedenti": 108.0,
+              "tempo_permanenza_sito": 18.47988348990493,
+              "percentuale_abbandono_carrello": 0.48687508605969587,
+              "prodotti_visualizzati": 299.0,
+              "interazioni_servizio_clienti": 0.0,
+              "numero_resi": 2.0,
+              "categorie_esplorate": 7.0,
+              "valore_lifetime": 287.77539647619284,
+              "orario_preferenziale": 10.203915755795173,
+              "tasso_apertura_email": 0.6007745530830877,
+              "numero_dispositivi": 3.0
             }
-        }
+          }
         ```
 
         Response:
         ```
         {
             "user_id": "user123",
-            "propensity": 0.67
+            "propensity": 0.98
         }
         ```
     """
@@ -204,7 +220,7 @@ async def health_check():
 
 @app.get("/readiness", response_model=ReadinessResponse)
 async def readiness_check():
-    joblib.load('model/logistic_regression_model.pkl')
+    joblib.load(model_path)
     return ReadinessResponse(
         status="ready",
         model_loaded=True,
